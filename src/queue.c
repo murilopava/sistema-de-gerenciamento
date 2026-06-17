265fa99aef7b3e6e501af40c1a0a0f578eac2c28
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-06-17 16:09:18 -0300

    implementa fila dinamica de clientes

---
+++ src/queue.c
@@ -0,0 +1,99 @@
+#include "queue.h"
+
+void inicializarFila(Fila *f)
+{
+	f->inicio = NULL;
+	f->fim = NULL;
+	f->tamanho = 0;
+}
+
+void enfileirar(Fila *f, Cliente c)
+{
+	Cliente *novo = (Cliente *)malloc(sizeof(Cliente));
+
+	if (!novo)
+		return;
+
+	*novo = c;
+	novo->prox = NULL;
+
+	if (f->fim == NULL) {
+		f->inicio = novo;
+		f->fim = novo;
+	} else {
+		f->fim->prox = novo;
+		f->fim = novo;
+	}
+
+	f->tamanho++;
+}
+
+Cliente desenfileirar(Fila *f)
+{
+	Cliente vazio = {"", 0, 0, 0};
+
+	if (f->inicio == NULL)
+		return vazio;
+
+	Cliente *temp = f->inicio;
+	Cliente c = *temp;
+
+	f->inicio = f->inicio->prox;
+
+	if (f->inicio == NULL)
+		f->fim = NULL;
+
+	free(temp);
+	f->tamanho--;
+
+	return c;
+}
+
+Cliente proximoCliente(Fila *f)
+{
+	Cliente vazio = {"", 0, 0, 0};
+
+	if (f->inicio == NULL)
+		return vazio;
+
+	return *(f->inicio);
+}
+
+int tamanhoFila(Fila *f)
+{
+	return f->tamanho;
+}
+
+void carregarClientes(Fila *f, const char *arquivo)
+{
+	FILE *fp = fopen(arquivo, "r");
+
+	if (!fp)
+		return;
+
+	Cliente c;
+
+	while (
+	    fscanf(fp, " %99[^;];%d;%d;%d", c.nome, &c.dia, &c.mes, &c.ano) ==
+	    4) {
+		c.prox = NULL;
+		enfileirar(f, c);
+	}
+
+	fclose(fp);
+}
+
+void liberarFila(Fila *f)
+{
+	Cliente *aux = f->inicio;
+
+	while (aux != NULL) {
+		Cliente *temp = aux;
+		aux = aux->prox;
+		free(temp);
+	}
+
+	f->inicio = NULL;
+	f->fim = NULL;
+	f->tamanho = 0;
+}
---
+++ src/queue.h
@@ -0,0 +1,31 @@
+#ifndef FILA_H
+#define FILA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Cliente {
+	char nome[100];
+	int dia;
+	int mes;
+	int ano;
+
+	struct Cliente *prox;
+} Cliente;
+
+typedef struct {
+	Cliente *inicio;
+	Cliente *fim;
+	int tamanho;
+} Fila;
+
+/* Funções */
+void inicializarFila(Fila *f);
+void enfileirar(Fila *f, Cliente c);
+Cliente desenfileirar(Fila *f);
+Cliente proximoCliente(Fila *f);
+int tamanhoFila(Fila *f);
+void carregarClientes(Fila *f, const char *arquivo);
+void liberarFila(Fila *f);
+
+#endif
